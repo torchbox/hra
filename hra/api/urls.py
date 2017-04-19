5917978a0052e39c72da6c4f690bd91dadfe1fc3
Mikalai Radchuk <radchuk.m@gmail.com>
2017-04-19 18:17:48 +0300

    Never cache API

--- hra/api/urls.py
+++ hra/api/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.views.decorators.cache import never_cache
+from wagtail.utils.urlpatterns import decorate_urlpatterns
 
 from hra.api.documentation import include_docs_urls
 from hra.api.routers import api_router
@@ -7,6 +9,8 @@ from hra.api.routers import api_router
 urlpatterns = [
     url(r'^api/v2/', include(api_router.urls)),
 ]
+
+urlpatterns = decorate_urlpatterns(urlpatterns, never_cache)
 
 urlpatterns += [
     url(r'^api_docs/', include_docs_urls(title='HRA API docs', patterns=urlpatterns)),
