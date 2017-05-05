f608de8d41c25e1882eaf014700ac6e551808a4d
Mikalai Radchuk <radchuk.m@gmail.com>
2017-05-05 17:00:46 +0100

    Startiing v1.10 Wagtail registers singlar handlers
    There is no need now to register signal handlers
    for custom image models anymore

--- hra/images/models.py
+++ hra/images/models.py
@@ -1,9 +1,5 @@
-from django.conf import settings
 from django.db import models
-from django.db.models.signals import pre_delete, pre_save
-from django.dispatch import receiver
-from wagtail.wagtailimages.models import (AbstractImage, AbstractRendition,
-                                          Image)
+from wagtail.wagtailimages.models import AbstractImage, AbstractRendition, Image
 
 
 # We define our own custom image class to replace wagtailimages.Image,
@@ -25,23 +21,6 @@ class CustomImage(AbstractImage):
         super().save(*args, **kwargs)
 
 
-# Do feature detection when a user saves an image without a focal point
-@receiver(pre_save, sender=CustomImage)
-def image_feature_detection(sender, instance, **kwargs):
-    if settings.WAGTAILIMAGES_FEATURE_DETECTION_ENABLED:
-        # Make sure the image doesn't already have a focal point
-        if not instance.has_focal_point():
-            # Set the focal point
-            instance.set_focal_point(instance.get_suggested_focal_point())
-
-
-# Receive the pre_delete signal and delete the file associated with the model instance.
-@receiver(pre_delete, sender=CustomImage)
-def image_delete(sender, instance, **kwargs):
-    # Pass false so FileField doesn't save the model.
-    instance.file.delete(False)
-
-
 class Rendition(AbstractRendition):
     image = models.ForeignKey(
         CustomImage,
@@ -53,10 +32,3 @@ class Rendition(AbstractRendition):
         unique_together = (
             ('image', 'filter_spec', 'focal_point_key'),
         )
-
-
-# Receive the pre_delete signal and delete the file associated with the model instance.
-@receiver(pre_delete, sender=Rendition)
-def rendition_delete(sender, instance, **kwargs):
-    # Pass false so FileField doesn't save the model.
-    instance.file.delete(False)
