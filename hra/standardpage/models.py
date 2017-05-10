fdded98342dddeb5c07ae683783480be02e6aa94
Mikalai Radchuk <radchuk.m@gmail.com>
2017-05-10 14:03:26 +0100

    Within this section block

--- hra/standardpage/models.py
+++ hra/standardpage/models.py
@@ -64,18 +64,7 @@ class StandardIndex(Page, SocialFields):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        subpages = self.get_children().live()
-        per_page = settings.DEFAULT_PER_PAGE
-        page_number = request.GET.get('page')
-        paginator = Paginator(subpages, per_page)
 
-        try:
-            subpages = paginator.page(page_number)
-        except PageNotAnInteger:
-            subpages = paginator.page(1)
-        except EmptyPage:
-            subpages = paginator.page(paginator.num_pages)
-
-        context['subpages'] = subpages
+        context['siblings'] = self.get_siblings().live().public()
 
         return context
