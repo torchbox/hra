fe773c376c1afaa39ef2c8f13fead5fe1697979a
Mikalai Radchuk <radchuk.m@gmail.com>
2017-05-10 14:28:59 +0100

    Adds additional fields to standard index pages
    1. Add hero introduction text field
    2. Add section choosers
    3. Add call to action snippet chooser

--- hra/standardpage/models.py
+++ hra/standardpage/models.py
@@ -11,18 +11,15 @@ from wagtail.wagtailadmin.edit_handlers import (
 from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsearch import index
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 
 from hra.utils.blocks import StoryBlock
 from hra.utils.models import (
     ListingFields,
     SocialFields,
     RelatedDocument,
-    RelatedPage
-)
-
-
-class StandardPageRelatedDocument(RelatedDocument):
-    page = ParentalKey('standardpage.StandardPage', related_name='related_documents')
+    RelatedPage,
+    CallToActionSnippet)
 
 
 class StandardPageRelatedPage(RelatedPage):
@@ -41,19 +38,28 @@ class StandardPage(Page, SocialFields, ListingFields):
     content_panels = Page.content_panels + [
         FieldPanel('introduction'),
         StreamFieldPanel('body'),
-        # TODO: Cleanup if we decide that we don't need related docs here
-        # InlinePanel('related_documents', label="Related documents"),
         InlinePanel('related_pages', label="Related pages"),
     ]
 
     promote_panels = Page.promote_panels + SocialFields.promote_panels + ListingFields.promote_panels
 
 
+class StandardIndexSectionPage(RelatedPage):
+    source_page = ParentalKey('standardpage.StandardIndex', related_name='section_pages')
+
+
 class StandardIndex(Page, SocialFields):
+    hero_introduction = models.CharField(blank=True, max_length=120,
+                                         help_text='Short text to appear under page title')
     introduction = models.TextField(blank=True)
+    call_to_action = models.ForeignKey(CallToActionSnippet, blank=True, null=True, on_delete=models.SET_NULL,
+                                       related_name='+')
 
     content_panels = Page.content_panels + [
+        FieldPanel('hero_introduction'),
         FieldPanel('introduction'),
+        SnippetChooserPanel('call_to_action'),
+        InlinePanel('section_pages', label='Section pages'),
     ]
 
     search_fields = Page.search_fields + [
