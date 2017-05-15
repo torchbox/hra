96170b4a70f57f619627877a8876dea786aa69b6
Mikalai Radchuk <radchuk.m@gmail.com>
2017-05-15 12:35:44 +0100

    Add body to contact form pages

--- hra/forms/models.py
+++ hra/forms/models.py
@@ -25,6 +25,7 @@ class FormField(AbstractFormField):
 
 class FormPage(WagtailCaptchaEmailForm, SocialFields, ListingFields):
     introduction = models.TextField(blank=True)
+    body = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True, help_text="Text displayed to the user on successful submission of the form")
     action_text = models.CharField(max_length=32, blank=True, help_text="Form action text. Defaults to \"Submit\"")
 
@@ -34,6 +35,7 @@ class FormPage(WagtailCaptchaEmailForm, SocialFields, ListingFields):
 
     content_panels = Page.content_panels + [
         FieldPanel('introduction'),
+        FieldPanel('body'),
         InlinePanel('form_fields', label="Form fields"),
         FieldPanel('action_text'),
         FieldPanel('thank_you_text'),
