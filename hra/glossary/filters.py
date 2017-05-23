4c2cc1411adfd07a0302ff7347686f32b4221b01
Mikalai Radchuk <radchuk.m@gmail.com>
2017-05-23 12:33:41 +0100

    Remove coreapi support for now

--- hra/glossary/filters.py
+++ hra/glossary/filters.py
@@ -1,14 +1,8 @@
-import coreapi
-import coreschema
-from django.utils.encoding import force_text
-from django.utils.translation import ugettext_lazy as _
 from rest_framework.filters import BaseFilterBackend
 
 
 class GlossaryTermsStartsWithFilter(BaseFilterBackend):
     query_parameter = 'term_startswith'
-    query_title = _('First letters')
-    query_description = _('One or more first letter of a glossary term')
 
     def filter_queryset(self, request, queryset, view):
         """
@@ -25,16 +19,3 @@ class GlossaryTermsStartsWithFilter(BaseFilterBackend):
                 queryset = queryset.filter(name__istartswith=value)
 
         return queryset
-
-    def get_schema_fields(self, view):
-        return [
-            coreapi.Field(
-                name=self.query_parameter,
-                required=False,
-                location='query',
-                schema=coreschema.String(
-                    title=force_text(self.query_title),
-                    description=force_text(self.query_description)
-                )
-            )
-        ]
