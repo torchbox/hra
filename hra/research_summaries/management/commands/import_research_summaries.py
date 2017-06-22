356a7c77dcec6b442adda1db4e2e6c343450911d
Mikalai Radchuk <radchuk.m@gmail.com>
2017-06-22 16:50:24 +0100

    Improve import_research_summaries
    Now the command accepts arguments,
    so it's possible to import summaries for a specific period
    
    By default it imports summaries since the last import

--- hra/research_summaries/management/commands/import_research_summaries.py
+++ hra/research_summaries/management/commands/import_research_summaries.py
@@ -1,18 +1,48 @@
-from urllib import parse
-
 import logging
+
+from dateutil.parser import parse as parse_date
 from django.core.management import BaseCommand, CommandError
+from django.db import models
 from django.utils.datetime_safe import date
 
 from hra.research_summaries.api import fetch_for_dates
-from hra.research_summaries.models import ResearchSummariesIndexPage
-
+from hra.research_summaries.models import ResearchSummariesIndexPage, ResearchSummaryPage
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    help = (
+        "The command that imports reports since the last update. If date range is "
+        "specified, imports summaries for a given period."
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument('--date-from', type=str, dest='start_date', default=None)
+        parser.add_argument('--date-to', type=str, dest='end_date', default=None)
+
     def handle(self, *args, **options):
+        start_date = options['start_date']
+        end_date = options['end_date']
+
+        if not (start_date and end_date) and (start_date or end_date):
+            raise CommandError(
+                "You must specify both --date-from and --date-to "
+                "to import research summaries for a given period."
+            )
+
+        if start_date and end_date:
+            # Use dates provided in the arguments
+            start_date = parse_date(start_date).date()
+            end_date = parse_date(end_date).date()
+        else:
+            # If there is no dates in arguments,
+            # use the date of the last update as a start date,
+            # and the current date as an end date
+            end_date = date.today()
+            start_date = ResearchSummaryPage.objects.aggregate(last_updated_at=models.Max('updated_at'))
+            start_date = start_date.get('last_updated_at').date()
+
         parent_page = ResearchSummariesIndexPage.objects.first()
 
         if not parent_page:
@@ -20,9 +50,5 @@ class Command(BaseCommand):
                 "There is no ResearchSummariesIndexPage pages. "
                 "You have to create one to be able to import research summaries."
             )
-
-        # TODO: Use arguments
-        start_date = date(2015, 12, 1)
-        end_date = date(2015, 12, 31)
 
         fetch_for_dates(parent_page, start_date, end_date)
