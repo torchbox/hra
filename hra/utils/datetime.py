43958cf3fc38ce4fb06957f62bd57a39a8d04117
Mikalai Radchuk <radchuk.m@gmail.com>
2017-06-23 11:09:47 +0100

    Add an utility function to iterate between dates

--- hra/utils/datetime.py
+++ hra/utils/datetime.py
@@ -1,7 +1,12 @@
+from dateutil.relativedelta import relativedelta
 from django.utils.datetime_safe import date
 
 
 def range_month(start_date, end_date):
+    """
+    Generates range of a months from `start_date` to `end_date`.
+    """
+
     current_year = start_date.year
     current_month = start_date.month
 
@@ -15,3 +20,26 @@ def range_month(start_date, end_date):
             current_month += 1
 
         yield date(current_year, current_month, 1)
+
+
+def iter_between_dates(start_date, end_date, step_days):
+    """
+    Generates range of dates from `start_date` to `end_date`.
+    Each step will be no longer than `step_days` days.
+    """
+    def increment_date():
+        result = current_start_date + relativedelta(days=step_days)
+
+        if result > end_date:
+            result = end_date
+
+        return result
+
+    current_start_date = start_date
+    current_end_date = increment_date()
+    yield current_start_date, current_end_date
+
+    while current_end_date < end_date:
+        current_start_date = current_end_date + relativedelta(days=1)
+        current_end_date = increment_date()
+        yield current_start_date, current_end_date
