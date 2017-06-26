900e5fdcd453443bb138324f43f66aa366e41592
Mikalai Radchuk <radchuk.m@gmail.com>
2017-06-26 11:30:26 +0100

    Add research summaries import cron

--- hra/wsgi_cron.py
+++ hra/wsgi_cron.py
@@ -33,6 +33,24 @@ if has_uwsgi:
     def update_index(signum):
         single_instance_command('update_index')
 
+    def import_research_summaries(signum):
+        """
+        Currently the API has limited availability,
+        it is open only during on these days and time:
+        Monday, Wednesday, Thursday – between 6:00 - 8:00 am UTC
+
+        Note that server is London time, so we have to run this cron
+        between 7am and 9am.
+        """
+        single_instance_command('import_research_summaries')
+
+    # Monday at 7:30am London time (6:30am UTC)
+    cron(30, 7, -1, -1, 1, target='mule')(import_research_summaries)
+    # Wednesday at 7:30am London time (6:30am UTC)
+    cron(30, 7, -1, -1, 3, target='mule')(import_research_summaries)
+    # Thursday at 7:30am London time (6:30am UTC)
+    cron(30, 7, -1, -1, 4, target='mule')(import_research_summaries)
+
     @timer(300)
     def publish_scheduled_pages(cron):
         single_instance_command('publish_scheduled_pages')
