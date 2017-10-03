3106ba86092b719e1398b7b1a6081a4f5a4d299e
Chris Lawton <chris.lawton@torchbox.com>
2017-10-03 11:46:36 +0100

    allowing for multiple datepickers on one page

--- hra/patternlab/site/javascript/components/datepicker.js
+++ hra/patternlab/site/javascript/components/datepicker.js
@@ -1,11 +1,16 @@
 import { TinyDatePicker } from '../globals';
 
 function datepicker() {
-    TinyDatePicker('.js-datepicker', {
-        format(date) {
-            return date.toLocaleDateString();
-        },
-    });
+
+    const datepickers = document.querySelectorAll('.js-datepicker');
+
+    for (let i = 0; i < datepickers.length; i++) {
+        TinyDatePicker(datepickers[i], {
+            format(date) {
+                return date.toLocaleDateString('en-GB');
+            },
+        });
+    }
 }
 
 export default datepicker;
