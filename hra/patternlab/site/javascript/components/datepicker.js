ecf88d3bc888b5e6271ffa8d9737f677752b35d6
Chris Lawton <chris.lawton@torchbox.com>
2017-10-04 13:32:14 +0100

    adding pikaday to datepicker input fields and overriding minimal styling

--- hra/patternlab/site/javascript/components/datepicker.js
+++ hra/patternlab/site/javascript/components/datepicker.js
@@ -1,7 +1,16 @@
+import Pikaday from 'pikaday';
 
 function datepicker() {
 
+    const dateFrom = new Pikaday({
+        field: document.getElementById('date_from'),
+        format: 'DD/MM/YYYY',
+    });
 
+    const dateTo = new Pikaday({
+        field: document.getElementById('date_to'),
+        format: 'DD/MM/YYYY',
+    });
 }
 
 export default datepicker;
